02eb56c52f4c834e469fb126df9f2093b1f4912e
sudalaimani <93456174+sudalaimanis@users.noreply.github.com>
2024-04-23 08:33:45 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull sudalaimmanis/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 sudalaimmanis/simple-python-flask-app
